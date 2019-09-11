84da1fcf2ab19ee41a6f2c7d864497e0f056330f
jiangxingyu <futuresoul00@gmail.com>
2019-09-11 00:13:30 +0000

    hhh

--- randomrun.py
+++ randomrun.py
@@ -13,8 +13,8 @@ pre_lines=f_test.readlines()
 new_pre_lines=[]
 for line in pre_lines:
     line = re.sub(" +", " ", line)
-    tokens = word_tokenize(line)
-    line = " ".join([i for i in tokens if i not in ['A','a','The','THE','the']])
+    #tokens = word_tokenize(line)
+    #line = " ".join([i for i in tokens if i not in ['A','a','The','THE','the']])
 
     new_pre_lines.append(line)
 
@@ -31,8 +31,8 @@ pos = int(0.66*len(lines))
 for line2 in lines:
         line2 = re.sub(" +", " ", line2)
         arr = line2.split("\t")
-        tokens = word_tokenize(arr[1])
-        arr[1] = " ".join([i for i in tokens if i not in ['A','a','The','THE','the']])
+        #tokens = word_tokenize(arr[1])
+        #arr[1] = " ".join([i for i in tokens if i not in ['A','a','The','THE','the']])
 
         newText = "__label__" + arr[0] + " " + arr[1]
         new_lines.append(newText)
@@ -54,16 +54,20 @@ for i in range(15):
     f_labeled.close()
     model = None
     if i%2 == 0:
+        print("mod1")
         model = fasttext.train_supervised(
-            input="new_train_tweets_all.txt", wordNgrams=2, ws=5, verbose=2, thread=7, t=0.0001, neg=5, minn=3,
+            input="new_train_tweets_all.txt", wordNgrams=2, ws=5, verbose=2, thread=15, t=0.0001, neg=5, minn=2,
+            minCountLabel=0, minCount=1, maxn=5, lrUpdateRate=100, epoch=30, lr=2.7185326849317737, bucket=2534640, dim=107)
+    elif i%2 == 1 and i<=2:
+        print("mod2")
+        model = fasttext.train_supervised(
+            input="new_train_tweets_all.txt", wordNgrams=2, ws=5, verbose=2, thread=15, t=0.0001, neg=5, minn=3,
             minCountLabel=0, minCount=1, maxn=6, lrUpdateRate=100, epoch=30, lr=2.2389885442286235, bucket=1084286, dim=133
         )
-    else:
+    elif i%2 == 1:
+        print("mod3")
         model = fasttext.train_supervised(
-            input="new_train_tweets_all.txt", wordNgrams=2, ws=5, verbose=2, thread=7, t=0.0001, neg=5, minn=2,
-            minCountLabel=0, minCount=1, maxn=5, lrUpdateRate=100, epoch=30, lr=2.7185326849317737, bucket=2534640,
-            dim=107
-        )
+            input="new_train_tweets_all.txt", wordNgrams= 2 , ws= 5 ,verbose= 2 ,thread= 15 ,t= 0.0001 ,neg= 5 ,minn= 2 ,minCountLabel= 0 ,minCount= 1 ,maxn= 5 ,lrUpdateRate= 100 ,epoch= 40 , lr= 1.8879426931318635 , bucket= 2342751 ,dim= 251) 
     # model.save_model("model/fasttext_"+str(i)+".ftz")
     if i>5:
         pos=int(0.5*len(lines))
@@ -77,7 +81,7 @@ for i in range(15):
     start predict
     '''
     result=[]
-    result_file = open("result/smltest_"+str(i)+".csv", "w", newline="")
+    result_file = open("result/smltest_nospl_"+str(i)+".csv", "w", newline="")
     writer = csv.writer(result_file)
     writer.writerow(["Id","Predicted"])
     id = 1
@@ -86,4 +90,4 @@ for i in range(15):
 
     writer.writerows(result)
 
-
+    result_file.close()
